59e1b6747163f0455c3a409d9ad758dd9b8e9bfa
Akhil reddy Putti reddy <a0p0ic5@homeoffice.wal-mart.com>
2022-07-27 12:48:32 +0530

    checkicon

--- todo.js
+++ todo.js
@@ -1,11 +1,19 @@
-const createevent = document.getElementById("addbtn");
+const addevent = document.getElementById("addbtn");
 const list = document.getElementById("task");
-const icon = document.getElementsByClassName("fa-solid fa-trash-can")[0];
-const search = document.getElementById("searchbar");
+const deletebtn = document.getElementsByClassName("fa-solid fa-trash-can")[0];
+const searchbtn = document.getElementById("searchbar");
+const checkicon = document.getElementsByClassName(
+  "fa-solid fa-square-check"
+)[0];
+const uncheckicon = document.getElementsByClassName(
+  "fa-solid fa-square-xmark"
+)[0];
 
-createevent.addEventListener("click", addtask);
-icon.addEventListener("click", deletetask);
-search.addEventListener("keyup", searchtask);
+addevent.addEventListener("click", addtask);
+deletebtn.addEventListener("click", deletetask);
+searchbtn.addEventListener("keyup", searchtask);
+checkicon.addEventListener("click", checktask);
+uncheckicon.addEventListener("click", unchecktask);
 
 function addtask(e) {
   const item = document.getElementById("item");
@@ -13,20 +21,29 @@ function addtask(e) {
   const options = document.createElement("li");
   li.className = "liitems";
   li.appendChild(document.createTextNode(item.value));
+  //delete button
   const deletebtn = document.createElement("i");
   deletebtn.className = "fa-solid fa-trash-can";
   options.appendChild(deletebtn);
+  deletebtn.addEventListener("click", deletetask);
+  //check button
   const checkbtn = document.createElement("i");
   checkbtn.className = "fa-solid fa-square-check";
   options.appendChild(checkbtn);
+  checkbtn.addEventListener("click", checktask);
+  //uncheckbutton
+  const uncheckbtn = document.createElement("i");
+  uncheckbtn.className = "fa-solid fa-square-xmark";
+  uncheckbtn.id = "uncheck";
+  options.appendChild(uncheckbtn);
+  uncheckbtn.addEventListener("click", unchecktask);
   li.appendChild(options);
   list.appendChild(li);
-  list.addEventListener("click", deletetask);
 }
 
 function deletetask(e) {
   if (confirm("are you sure")) {
-    const li = e.target.parentElement;
+    const li = e.target.parentElement.parentElement;
     list.removeChild(li);
   }
 }
@@ -43,4 +60,23 @@ function searchtask(e) {
       items.style.display = "none";
     }
   });
+}
+
+function checktask(e) {
+  const task = e.target.parentElement.parentElement;
+  const icons = e.target.parentElement;
+  const self = e.target;
+  task.style.backgroundColor = "green";
+  icons.style.backgroundColor = "green";
+  self.style.display = "none";
+  icons.display = "flex";
+}
+
+function unchecktask(e) {
+  const task = e.target.parentElement.parentElement;
+  const icons = e.target.parentElement;
+  task.style.backgroundColor = "rgb(130,232,171)";
+  icons.style.backgroundColor = "rgb(130,232,171)";
+  checkicon.style.display = "flex";
+  uncheckicon.style.display = "none";
 }
